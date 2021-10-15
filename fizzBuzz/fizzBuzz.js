4acb3e62f36d3e33141459c26698deca2e13eb48
Raymond Nhim <raymondnh@function-HotSoz.localdomain>
2021-10-15 14:31:48 -0700

    added new toy problems

---
+++ fizzBuzz/fizzBuzz.js
@@ -0,0 +1,30 @@
+/*  
+
+Given an integer n return a string array which will be called answer (one indexed) 
+Where answer[i] equals "FizzBuzz" if i is divisable by 3 and 5
+Where answer[i] equals "Fizz" if i is divisable by 3
+Where answer[i] equals "Buzz" if i is divisable by 5
+Where answer[i] equals i if none of the above conditions are true
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+*/
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+var fizzBuzz = (n) => {
+
+}
